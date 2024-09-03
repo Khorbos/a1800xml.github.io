b9faf78847d02fbb55612877c18abab78909022a
pnski <s1l3nce@hotmail.de>
2024-09-03 21:10:57 +0200

    _u

--- search.js
+++ search.js
@@ -21,14 +21,14 @@ function resultDisplay(results) {
 }
 
 function formatXml(xml, tab) { // tab = optional indent value, default is tab (\t)
-   /*  var formatted = '', indent= '';
+    var formatted = '', indent= '';
     tab = tab || '\t';
     xml.split(/>\s*</).forEach(function(node) {
         if (node.match( /^\/\w/ )) indent = indent.substring(tab.length); // decrease indent by one 'tab'
         formatted += indent + '<' + node + '>\r\n';
         if (node.match( /^<?\w[^>]*[^\/]$/ )) indent += tab;              // increase indent
     });
-    return formatted.substring(1, formatted.length-3); */
+    return formatted.substring(1, formatted.length-3);
     const options = {
         processEntities:false,
         format: true,
@@ -36,7 +36,8 @@ function formatXml(xml, tab) { // tab = optional indent value, default is tab (\
         cdataPropName: "phone"
     };
     const builder = new fxparser.XMLBuilder(options);
-    return builder.build(xml);
+    const chars = builder.build(xml);
+	return chars;
 }
 
 async function getXML(file) {
